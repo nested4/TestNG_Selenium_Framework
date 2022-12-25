2820a7048afceb8dcfcd419269912a14cbf71e46
ihsan <111647146+nested4@users.noreply.github.com>
2022-12-25 21:15:22 +0300

    ihsan2 branch day7 added

--- src/test/java/com/krafttechnologie/tests/day10_webElements4/IframeTest.java
+++ src/test/java/com/krafttechnologie/tests/day10_webElements4/IframeTest.java
@@ -8,12 +8,15 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class IframeTest {
     WebDriver driver;
     @BeforeMethod
     public void setUp() {
         driver = WebDriverFactory.getDriver("chrome");
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
     @AfterMethod
     public void tearDown() throws InterruptedException {
