ae2250ae74b01d74876a8114c04ef37174125647
ihsan <111647146+nested4@users.noreply.github.com>
2022-12-25 20:55:03 +0300

    ihsan2 branch day7 added

---
+++ src/test/java/com/krafttechnologie/tests/day07_TestNG/TestNGAnnotationDemo.java
@@ -0,0 +1,81 @@
+package com.krafttechnologie.tests.day07_TestNG;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestNGAnnotationDemo {
+
+    @BeforeMethod
+    public void setUp(){
+        System.out.println("Open Browser");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("First Test First Assertion");
+        Assert.assertEquals("tiTle","title");
+
+
+        System.out.println("First Test Second Assertion");
+        Assert.assertEquals("url","url");
+
+
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Second Test Assertion");
+        Assert.assertEquals("test2","test2");
+
+    }
+    @Test
+    public void test3(){
+        String expectedTitle="Kraft";
+        String actualTitle="Kraftech";
+
+        Assert.assertTrue(actualTitle.contains(expectedTitle),"verify that title starting Kraft");
+    }
+
+
+    @Test
+    public void test4(){
+        //verify that email contains @ sign
+        String email="kraftTech@gmail.com";
+
+//        if (email.contains("@")){
+//            System.out.println("PASS");
+//        }else{
+//            System.out.println("FAILED");
+//        }
+
+        Assert.assertTrue(email.contains("@"));
+
+    }
+
+    @Test
+    public void test5(){
+
+        Assert.assertFalse(0>1,"verify that = is not greater than 1");
+
+    }
+
+    @Test
+    public void test6(){
+        Assert.assertNotEquals("two","one");
+    }
+
+
+
+
+
+
+    @AfterMethod
+    public void tearDown(){
+        System.out.println("Close Browser");
+    }
+
+
+
+}
