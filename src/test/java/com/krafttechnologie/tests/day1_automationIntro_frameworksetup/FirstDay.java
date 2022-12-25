f0df0d165320fc75b733467d41a062e175268ec4
ihsan <111647146+nested4@users.noreply.github.com>
2022-12-25 19:37:39 +0300

    Project Structure is built, day1 day2 codes are added

---
+++ src/test/java/com/krafttechnologie/tests/day1_automationIntro_frameworksetup/FirstDay.java
@@ -0,0 +1,9 @@
+package com.krafttechnologie.tests.day1_automationIntro_frameworksetup;
+
+public class FirstDay {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Test Automation");
+    }
+
+}
